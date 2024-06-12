44d0f6eb4bb6b969ac887ba119c52befa62e1daa
lenorcon <lenorcon458@gmail.com>
2024-06-13 07:07:12 +0800

    another commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,9 +13,9 @@ const Navbar = () => {
             <div className="bg-black text-gray-400 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center">
                 <h1 className="text-3xl font-bold primary-color ml-4">Lenorcon</h1>
                 <ul className="hidden md:flex ">
-                    <li className="p-5 hover:text-gray-700"><a href="#about">About</a></li>
-                    <li className="p-5 hover:text-gray-700"><a href="#work">Work</a></li>
-                    <li className="p-5 hover:text-gray-700"><a href="#contact">Contact</a></li>
+                    <li className="p-5 hover:text-gray-700 font-bold"><a href="#about">About</a></li>
+                    <li className="p-5 hover:text-gray-700 font-bold"><a href="#work">Work</a></li>
+                    <li className="p-5 hover:text-gray-700 font-bold"><a href="#contact">Contact</a></li>
                 </ul>
                 <div onClick={handleNav} className="block md:hidden mr-6">
                     {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
